416b16b84aee6154c0c3db66d88b41136cca3263
AYUSHNSUT <ayushm1906@gmail.com>
2021-03-07 03:50:46 +0530

    FirstAPIProduced

---
+++ app.py
@@ -0,0 +1,44 @@
+# app.py
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+import pickle as pkl
+import sklearn
+import numpy as np
+
+@app.route('/covid/', methods=['GET'])
+def respond():
+    # Retrieve the arguements from url parameter
+    receivedDict = request.args
+    # For debugging
+    # print(f"receivedDict {receivedDict}")
+
+    rawList = []
+
+    for item in receivedDict:
+        rawList.append(receivedDict[item])
+
+    clf = pkl.load(open('covidmodel', 'rb'))
+    inputVector = np.array(rawList)
+    inputVector = inputVector.reshape(1,-1)
+
+    y = clf.predict(inputVector)
+
+    ans = ""
+
+    if(y[0] == 0):
+        ans = "0"
+    else:
+        ans = "1"
+
+    
+    # Return the response in json format
+    return ans
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
