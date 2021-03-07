bccfac1f2fbbaf83d9e41bb1b2fcab15f7230096
AYUSHNSUT <ayushm1906@gmail.com>
2021-03-08 02:04:39 +0530

    Medicine data scraper api added

--- app.py
+++ app.py
@@ -4,6 +4,65 @@ app = Flask(__name__)
 import pickle as pkl
 import sklearn
 import numpy as np
+import requests
+import re
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+
+@app.route('/medicine',methods=['GET'])
+def isabel():
+	if request.method == 'GET':
+		i=request.args.get("medicine_name")
+
+
+		#condition if any of the params is missing
+		if i==None :
+			return jsonify({'error':'name of medicine is missing!!'}),400
+
+		SAUCE_USERNAME = 'kapishgarg0214'
+		SAUCE_ACCESS_KEY = '3cf56841-a783-4661-8f91-27bbe9aa5c35'
+
+		print("starting the session")
+		driver = webdriver.Remote(
+		desired_capabilities=webdriver.DesiredCapabilities.FIREFOX,
+		command_executor='http://%s:%s@ondemand.saucelabs.com:80/wd/hub' %
+		(SAUCE_USERNAME, SAUCE_ACCESS_KEY)
+		)
+
+		driver.get("https://www.1mg.com/search/all?name="+str(i))
+
+		data=driver.page_source
+
+		driver.quit()
+
+		soup=BeautifulSoup(data,'html5lib')
+		prices=[]
+		names=[]
+		main_container = soup.find_all("div", {"class": "row style__grid-container___3OfcL"})
+		if(len(main_container)==0):
+			 return jsonify([],[]),200
+		if(len(main_container)>=2):
+			all_drugs = main_container[1].find_all("div", {"class": "col-xs-12 col-md-9 col-sm-9 style__container___cTDz0"})
+		else:
+			all_drugs = main_container[0].find_all("div", {"class": "col-xs-12 col-md-9 col-sm-9 style__container___cTDz0"})
+		for drug in all_drugs:
+			drug_name = drug.find("div", {"class": "style__product-description___1vPQe"}).text
+			price = drug.find("div", {"class": "style__price-tag___B2csA"}).text
+			price = re.sub('[^0-9.]', "", price)
+			names.append(drug_name)
+			prices.append(price)
+		ans=[names,prices]		
+		return jsonify(ans),200
+
+
 
 @app.route('/covid/', methods=['GET'])
 def respond():
