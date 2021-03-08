5e32b252bd2623fcc9f2facad0761035cdae8eca
AYUSHNSUT <ayushm1906@gmail.com>
2021-03-08 11:52:25 +0530

    Parser library added

--- app.py
+++ app.py
@@ -1,67 +1,70 @@
 # app.py
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import re
+import requests
+import numpy as np
+import sklearn
+import pickle as pkl
 from flask import Flask, request, jsonify
 app = Flask(__name__)
-import pickle as pkl
-import sklearn
-import numpy as np
-import requests
-import re
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import time
 
 
-@app.route('/medicine',methods=['GET'])
+@app.route('/medicine', methods=['GET'])
 def isabel():
-	if request.method == 'GET':
-		i=request.args.get("medicine_name")
+    if request.method == 'GET':
+        i = request.args.get("medicine_name")
+        print(i)
+        # condition if any of the params is missing
+        if i == None:
+            return jsonify({'error': 'name of medicine is missing!!'}), 400
 
+        SAUCE_USERNAME = 'kapishgarg0214'
+        SAUCE_ACCESS_KEY = '3cf56841-a783-4661-8f91-27bbe9aa5c35'
 
-		#condition if any of the params is missing
-		if i==None :
-			return jsonify({'error':'name of medicine is missing!!'}),400
+        print("starting the session")
+        driver = webdriver.Remote(
+            desired_capabilities=webdriver.DesiredCapabilities.FIREFOX,
+            command_executor='http://%s:%s@ondemand.saucelabs.com:80/wd/hub' %
+            (SAUCE_USERNAME, SAUCE_ACCESS_KEY)
+        )
 
-		SAUCE_USERNAME = 'kapishgarg0214'
-		SAUCE_ACCESS_KEY = '3cf56841-a783-4661-8f91-27bbe9aa5c35'
+        driver.get("https://www.1mg.com/search/all?name="+str(i))
 
-		print("starting the session")
-		driver = webdriver.Remote(
-		desired_capabilities=webdriver.DesiredCapabilities.FIREFOX,
-		command_executor='http://%s:%s@ondemand.saucelabs.com:80/wd/hub' %
-		(SAUCE_USERNAME, SAUCE_ACCESS_KEY)
-		)
+        data = driver.page_source
 
-		driver.get("https://www.1mg.com/search/all?name="+str(i))
+        driver.quit()
 
-		data=driver.page_source
-
-		driver.quit()
-
-		soup=BeautifulSoup(data,'html5lib')
-		prices=[]
-		names=[]
-		main_container = soup.find_all("div", {"class": "row style__grid-container___3OfcL"})
-		if(len(main_container)==0):
-			 return jsonify([],[]),200
-		if(len(main_container)>=2):
-			all_drugs = main_container[1].find_all("div", {"class": "col-xs-12 col-md-9 col-sm-9 style__container___cTDz0"})
-		else:
-			all_drugs = main_container[0].find_all("div", {"class": "col-xs-12 col-md-9 col-sm-9 style__container___cTDz0"})
-		for drug in all_drugs:
-			drug_name = drug.find("div", {"class": "style__product-description___1vPQe"}).text
-			price = drug.find("div", {"class": "style__price-tag___B2csA"}).text
-			price = re.sub('[^0-9.]', "", price)
-			names.append(drug_name)
-			prices.append(price)
-		ans=[names,prices]		
-		return jsonify(ans),200
-
+        soup = BeautifulSoup(data, 'html5lib')
+        prices = []
+        names = []
+        main_container = soup.find_all(
+            "div", {"class": "row style__grid-container___3OfcL"})
+        if(len(main_container) == 0):
+            return jsonify([], []), 200
+        if(len(main_container) >= 2):
+            all_drugs = main_container[1].find_all(
+                "div", {"class": "col-xs-12 col-md-9 col-sm-9 style__container___cTDz0"})
+        else:
+            all_drugs = main_container[0].find_all(
+                "div", {"class": "col-xs-12 col-md-9 col-sm-9 style__container___cTDz0"})
+        for drug in all_drugs:
+            drug_name = drug.find(
+                "div", {"class": "style__product-description___1vPQe"}).text
+            price = drug.find(
+                "div", {"class": "style__price-tag___B2csA"}).text
+            price = re.sub('[^0-9.]', "", price)
+            names.append(drug_name)
+            prices.append(price)
+        ans = [names, prices]
+        return jsonify(ans), 200
 
 
 @app.route('/covid/', methods=['GET'])
@@ -78,7 +81,7 @@ def respond():
 
     clf = pkl.load(open('covidmodel', 'rb'))
     inputVector = np.array(rawList)
-    inputVector = inputVector.reshape(1,-1)
+    inputVector = inputVector.reshape(1, -1)
 
     y = clf.predict(inputVector)
 
@@ -89,9 +92,9 @@ def respond():
     else:
         ans = "1"
 
-    
     # Return the response in json format
     return ans
+
 
 @app.route('/heart/', methods=['GET'])
 def respond_heart():
@@ -107,7 +110,7 @@ def respond_heart():
 
     clf = pkl.load(open('heartmodel', 'rb'))
     inputVector = np.array(rawList)
-    inputVector = inputVector.reshape(1,-1)
+    inputVector = inputVector.reshape(1, -1)
 
     y = clf.predict(inputVector)
 
@@ -118,16 +121,15 @@ def respond_heart():
     else:
         ans = "1"
 
-    
     # Return the response in json format
     return ans
-
 
 
 # A welcome message to test our server
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
